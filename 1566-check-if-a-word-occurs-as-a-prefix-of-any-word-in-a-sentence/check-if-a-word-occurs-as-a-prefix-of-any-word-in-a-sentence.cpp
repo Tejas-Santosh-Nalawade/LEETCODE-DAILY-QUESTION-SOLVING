3614abb64fb3f70f3fbbdf9cbd7e084625f4ba24
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2024-12-02 22:45:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (6.42%) - LeetSync

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -6,7 +6,9 @@ public:
         int wordPosition = 1;
         while (sentenceStream >> currentWord) {
             if (currentWord.length() >= searchWord.length() &&
-                currentWord.compare(0, searchWord.length(), searchWord) == 0) {
+                currentWord.compare(0, searchWord.length(),
+                 searchWord) == 0)
+                 {
 
                 return wordPosition;
             }

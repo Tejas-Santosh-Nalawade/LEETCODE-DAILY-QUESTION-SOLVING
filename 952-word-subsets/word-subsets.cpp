a27099ea21ca6102432748fa516a4aee9c1a3abd
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-01-10 22:48:54 +0530

    Time: 59 ms (57.53%) | Memory: 106.9 MB (33.79%) - LeetSync

---
+++ 952-word-subsets/word-subsets.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+vector<int> bmax(26, 0); 
+        for (const string& b : words2) {
+            vector<int> bCount = count(b);
+            for (int i = 0; i < 26; ++i) {
+                bmax[i] = max(bmax[i], bCount[i]);
+            }
+        }
+        
+        vector<string> ans;
+        for (const string& a : words1) {
+            vector<int> aCount = count(a);
+            bool valid = true;
+            for (int i = 0; i < 26; ++i) {
+                if (aCount[i] < bmax[i]) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                ans.push_back(a);
+            }
+        }
+        
+        return ans;
+    }
+    
+private:
+    vector<int> count(const string& S) {
+        vector<int> ans(26, 0);
+        for (char c : S) {
+            ans[c - 'a']++;
+        }
+        return ans;
+    }
+};

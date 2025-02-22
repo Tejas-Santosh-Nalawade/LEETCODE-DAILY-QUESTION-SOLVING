b976ed07c0710ebe3b839555051b7c987f372981
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-02-22 20:38:13 +0530

    Time: 3 ms (62.80%) | Memory: 20.6 MB (98.47%) - LeetSync

---
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    TreeNode* recoverFromPreorder(string traversal) {
+        vector<TreeNode*> levels;
+        int index = 0, n = traversal.size();
+
+        while (index < n) {
+            int depth = 0;
+            while (index < n && traversal[index] == '-') {
+                depth++;
+                index++;
+            }
+            int value = 0;
+            while (index < n && isdigit(traversal[index])) {
+                value = value * 10 + (traversal[index] - '0');
+                index++;
+            }
+            TreeNode* node = new TreeNode(value);
+            if (depth < levels.size()) {
+                levels[depth] = node;
+            } else {
+                levels.push_back(node);
+            }
+            if (depth > 0) {
+                TreeNode* parent = levels[depth - 1];
+                if (parent->left == nullptr) {
+                    parent->left = node;
+                } else {
+                    parent->right = node;
+                }
+            }
+        }
+        return levels[0];
+    }
+};

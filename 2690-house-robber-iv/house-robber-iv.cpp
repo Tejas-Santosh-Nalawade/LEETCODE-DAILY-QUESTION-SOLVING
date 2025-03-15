472f1df73006d2e84b6108e338b1120d9e235678
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-03-15 23:02:49 +0530

    Time: 20 ms (75.32%) | Memory: 60.9 MB (40.42%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.cpp
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int minCapability(vector<int>& nums, int k) {
-        int minReward = 1, maxReward = *max_element(nums.begin(), nums.end()),
-            totalHouses = nums.size();
+        int minReward = 1, maxReward = *max_element(nums.begin(), nums.end()),totalHouses = nums.size();
         while (minReward < maxReward) {
             int midReward = (minReward + maxReward) / 2;
             int possibleThefts = 0;
@@ -10,7 +9,7 @@ public:
             for (int index = 0; index < totalHouses; ++index) {
                 if (nums[index] <= midReward) {
                     possibleThefts += 1;
-                    index++;  
+                    index++;
                 }
             }
 

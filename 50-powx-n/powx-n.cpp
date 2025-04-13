7177f4548ca66d51aff9e686586f4966f543283c
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-04-13 11:20:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.93%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        double ans=1;
+        long long m=n;
+        if(n<0){
+            x=1/x;
+            m=-m;
+        }
+        while(m){
+            if(m%2==1){
+                ans*=x;
+            }
+            x*=x;
+            m /= 2;
+        }
+        return ans;
     }
 };

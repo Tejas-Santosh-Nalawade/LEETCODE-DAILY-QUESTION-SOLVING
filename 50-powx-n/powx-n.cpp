f0a9cfb8f1c9530738f497a4de5f056ee6c60b02
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-04-13 12:54:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (15.49%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    double solve(double x, long n){
+    double myPow(double x, long n) {
         if (n==0) return 1; 
         if(n<0){
-            return solve(1/x,-n);
+            return myPow(1/x,-n);
         }
         if(n%2==0){
-            return solve(x*x,n/2);
+            return myPow(x*x,n/2);
         }
         else{
-            return x*solve(x*x,(n-1)/2);
+            return x*myPow(x*x,(n-1)/2);
         }
-    }
-    double myPow(double x, int n) {
-        return solve(x,(long)n);
     }
 };

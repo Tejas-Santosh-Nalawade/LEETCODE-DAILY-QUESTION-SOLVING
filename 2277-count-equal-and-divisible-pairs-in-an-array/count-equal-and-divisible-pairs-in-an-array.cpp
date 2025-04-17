68c6689f010e92125d3d6f39d2c8801b10e89391
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-04-17 08:09:38 +0530

    Time: 3 ms (28.60%) | Memory: 16.1 MB (10.14%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
-        unordered_map<int, vector<int>> valueToIndices;
-        
-        // Group indices by the value
-        for (int i = 0; i < nums.size(); ++i) {
-            valueToIndices[nums[i]].push_back(i);
-        }
-        
+        int n = nums.size();
+        unordered_map<int, vector<int>> valToIndices;
         int count = 0;
-        
-        // For each group, apply two-pointer style over indices
-        for (auto& [val, indices] : valueToIndices) {
-            int n = indices.size();
-            for (int low = 0; low < n; ++low) {
-                for (int high = low + 1; high < n; ++high) {
-                    if ((indices[low] * indices[high]) % k == 0) {
-                        count++;
-                    }
+
+        for (int i = 0; i < n; ++i) {
+            int val = nums[i];
+            for (int prevIndex : valToIndices[val]) {
+                if ((prevIndex * i) % k == 0) {
+                    count++;
                 }
             }
+            valToIndices[val].push_back(i);
         }
-        
+
         return count;
     }
 };

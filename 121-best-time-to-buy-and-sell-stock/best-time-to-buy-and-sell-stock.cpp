50ab5eb81dd3e9b47c5934866741f5b251f7850e
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-05-03 12:35:13 +0530

    Time: 1 ms (37.52%) | Memory: 97.3 MB (87.14%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int bestBuy[100000];
-        bestBuy[0]=INT_MAX;
-        for(int i=1; i<prices.size(); i++)
-        {
-            bestBuy[i]=min(prices[i-1],bestBuy[i-1]);
-        }
-        int maxProfit=0;
-        for(int i=0; i<prices.size(); i++)
-        {
-            int currentProfit=prices[i] - bestBuy[i];
-            maxProfit = max(maxProfit, currentProfit);
-        }
-        return maxProfit;
+    //     int bestBuy[100000];
+    //     bestBuy[0]=INT_MAX;
+    //     for(int i=1; i<prices.size(); i++)
+    //     {
+    //         bestBuy[i]=min(prices[i-1],bestBuy[i-1]);
+    //     }
+    //     int maxProfit=0;
+    //     for(int i=0; i<prices.size(); i++)
+    //     {
+    //         int currentProfit=prices[i] - bestBuy[i];
+    //         maxProfit = max(maxProfit, currentProfit);
+    //     }
+    //     return maxProfit;
+    // }
+    int maxProfit=0, maxCurr=0; 
+    for(int i=1; i<prices.size(); i++)
+    {
+        maxCurr= max(0, maxCurr+=prices[i]- prices[i-1]);
+        maxProfit= max(maxCurr,maxProfit);
+    }
+    return maxProfit;
     }
 };

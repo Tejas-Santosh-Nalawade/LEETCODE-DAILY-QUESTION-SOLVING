631c2d9e6bef8404ea79f2053469b78c6083e641
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-06-05 19:41:36 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (21.67%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -7,14 +7,14 @@ public:
             int other = target - nums[i];
             if(hm.count(other))
             {
-                vector<int> res; 
-                return {i, hm[other]};
+                // vector<int> res; 
                 // res.push_back(hm[other]);
                 // res.push_back(i);
                 // return res;
+                return {i, hm[other]};
             }
             else{
-                hm[nums[i]] = i;
+                hm[nums[i]] = i; // hashmap key value add
             }
         }
         return {-1};

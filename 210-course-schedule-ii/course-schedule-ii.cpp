4bc10d4d0f759d112a27acb691d6d83a7da1c3f2
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-06-15 20:23:49 +0530

    Time: 3 ms (73.66%) | Memory: 18 MB (90.45%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -20,22 +20,35 @@ public:
 
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
+        vector<int> incoming(numCourses,0);
+
         // Adjacent List
         for (vector<int> &e : prerequisites) {
             int u = e[0], v = e[1];
             adj[v].push_back(u);
+            incoming[u]++;
+
         }
 
-        vector<int> vis(numCourses, 0);
-        vector<int> topo(numCourses);
-        topoIdx = numCourses -1;   
-        for(int idx = 0; idx<numCourses; idx++)
+        queue<int> q;
+        vector<int> topo;
+
+        for(int idx=0; idx<numCourses; idx++)
         {
-            if(!vis[idx]){
-                bool cycle =dfs(idx ,adj ,vis, topo);
-                if(cycle) return {};
-            } 
+            if(incoming[idx] == 0) q.push(idx);
         }
+
+        while(q.size() !=0){
+            int src  =q.front();
+            q.pop();
+            topo.push_back(src);
+
+            for(int &nbr : adj[src]){
+                incoming[nbr] --;
+                if(incoming[nbr] == 0) q.push(nbr);
+            }
+        }
+        if(topo.size() < numCourses) return {};
         return topo;
     }
 };

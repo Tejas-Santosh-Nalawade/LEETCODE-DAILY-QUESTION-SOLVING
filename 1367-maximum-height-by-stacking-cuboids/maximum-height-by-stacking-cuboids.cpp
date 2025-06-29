45567f534171bf9aac4759cc64c572b1e98b24e8
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-06-29 19:33:43 +0530

    Time: 9 ms (61.33%) | Memory: 15.2 MB (42.45%) - LeetSync

--- 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.cpp
+++ 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
 
-    int solve(vector<vector<int>>& cuboids, int prev, int idx){
+    int solve(vector<vector<int>>& cuboids, int prev, int idx ,vector<vector<int>>&dp){
         if(idx == cuboids.size()){
             return 0;
         }
+        if (dp[prev+1][idx]!= -1){
+            return dp[prev+1][idx];
+        }
         
-        int skip_idx = solve(cuboids,prev,idx+1);
+        int skip_idx = solve(cuboids,prev,idx+1,dp);
 
         int select = 0;
 
         if(prev ==-1 || cuboids[prev][0] <= cuboids[idx][0] && cuboids[prev][1] <= cuboids[idx][1] && cuboids[prev][2] <= cuboids[idx][2]){
-            select = cuboids[idx][2] + solve(cuboids,idx,idx+1);
+            select = cuboids[idx][2] + solve(cuboids,idx,idx+1,dp) ;
         }
-        return max(skip_idx, select);
+         dp[prev+1][idx] = max(skip_idx, select);
+         return dp[prev+1][idx];
     }
 
     int maxHeight(vector<vector<int>>& cuboids) {
         int n = cuboids.size();
+        vector<vector<int>> dp(n, vector<int> (n,-1));
         for(int i=0; i<n; i++){
             sort(cuboids[i].begin(), cuboids[i].end());
         }
         sort(cuboids.begin(), cuboids.end());
 
-        int ans = solve(cuboids, -1, 0);
+        int ans = solve(cuboids, -1, 0,dp);
         return ans;
     }
 };

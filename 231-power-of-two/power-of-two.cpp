d7f0847380a65e165f77fad24323b825139ff628
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-08-13 21:52:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (52.42%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
+    if (n <= 0) return false;
+    double x = log2(n);
+    return floor(x) == x;
     }
 };

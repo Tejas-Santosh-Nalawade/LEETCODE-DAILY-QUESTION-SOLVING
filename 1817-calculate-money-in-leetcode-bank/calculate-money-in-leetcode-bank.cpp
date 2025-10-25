fef62cb81843d09bce8e5847a21fd02f07b06747
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-10-25 19:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (53.24%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int ans = 0;
-        int monday = 1;
-        while(n>0){
-            for(int day=0; day<min(n,7);day++){
-                ans+=monday+day;
-            }
-            n-=7;
-            monday++;
+        // Total weeks 
+        int k = n/7;
+        // Difference in each week
+        int d = 7;
+        // First term Arithematic Sequence
+        int first_term = 28;
+        int last_term = first_term + (k-1)*7;
+
+        // First sequence were all weeks 7 days complete
+        int arithmaticSum = k * (first_term + last_term)/2;
+
+        int monday = k+1;
+        int finalweek = 0;
+
+        // Second Sequnce for the last week with < 7 days
+
+        for(int day=0; day<n % 7;day++){
+            finalweek+=monday+day;
         }
-        return ans;
+        // Sum of First Sequnce and last week
+        int total_money = finalweek + arithmaticSum;
+
+        return total_money;
+
     }
 };

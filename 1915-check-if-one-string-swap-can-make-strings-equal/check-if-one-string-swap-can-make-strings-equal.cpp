1ad4a8db1ed310dc6dcb04f53bcbde09ac0e5266
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-10-26 13:41:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (42.75%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        vector<int>diff_indices;
-        for(int i=0;i<s1.length();i++){
-            if(s1[i]!=s2[i]){
-                diff_indices.push_back(i);
+        int firstIndexDiff = 0;
+        int secondIndexDiff = 0;
+        int numDiffs = 0;
+        for (int i = 0; i < s1.size(); i++) {
+            if (s1[i] != s2[i]) {
+                numDiffs++;
+                // numDiffs is more than 2, one string swap will not make two
+                // strings equal
+                if (numDiffs > 2)
+                    return false;
+                else if (numDiffs == 1) {
+                    // store the index of first difference
+                    firstIndexDiff = i;
+                } else {
+                    // store the index of second difference
+                    secondIndexDiff = i;
+                }
             }
         }
-        if(diff_indices.empty()){
-            return true;
-        }
-
-        if(diff_indices.size() == 2){
-            int i = diff_indices[0];
-            int j = diff_indices[1];
-
-            if(s1[i] == s2[j] && s1[j] == s2[i]){
-                return true;
-            }
-        }
-        return false;
-
+        // check if swap is possible
+        return s1[firstIndexDiff] == s2[secondIndexDiff] &&
+               s1[secondIndexDiff] == s2[firstIndexDiff];
     }
 };

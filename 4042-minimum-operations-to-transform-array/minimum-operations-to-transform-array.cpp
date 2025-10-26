354f7b13f98d34fdc6cfc218519ac584fabbe382
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-10-26 17:55:28 +0530

    Time: 3 ms (81.55%) | Memory: 213.8 MB (0.00%) - LeetSync

--- 4042-minimum-operations-to-transform-array/minimum-operations-to-transform-array.cpp
+++ 4042-minimum-operations-to-transform-array/minimum-operations-to-transform-array.cpp
@@ -1,20 +1,24 @@
 class Solution {
-typedef long long ll;
 public:
     long long minOperations(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size(), n2 = n1 + 1;
+        long long sum = 0 ;
+        for(int i = 0 ; i < nums1.size() ; i++){
+            sum += abs(nums1[i] - nums2[i]);
+        }
+        int ele = nums2[nums1.size()];
         int mini = INT_MAX;
-        for(int i = 0; i < n1; i++) {
-            if(nums2[n2 - 1] >= nums2[i] and nums2[n2 - 1] <= nums1[i]) mini = 1;
-            else if (nums2[n2 - 1] <= nums2[i] and nums2[n2 - 1] >= nums1[i]) mini = 1;
-            else {
-                mini = min(mini, min(abs(nums2[i] - nums2[n2 - 1]) + 1, abs(nums1[i] - nums2[n2 - 1]) + 1));
+        for(int i = 0 ; i < nums1.size() ; i++){
+            int low = min(nums1[i],nums2[i]);
+            int high = max(nums1[i],nums2[i]);
+            if(ele >= low && ele <= high) return sum + 1;
+            else{
+                if(ele < low){
+                    mini = min(mini,low-ele);
+                }else if(ele > high){
+                    mini = min(mini,ele - high);
+                }
             }
         }
-        ll ans = mini;
-        for(int i = 0; i < n1; i++) {
-            ans += abs(nums1[i] - nums2[i]);
-        }
-        return ans;
+        return sum + mini + 1;
     }
 };

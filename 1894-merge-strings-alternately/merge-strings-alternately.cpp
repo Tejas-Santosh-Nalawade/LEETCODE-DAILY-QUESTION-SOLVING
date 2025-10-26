e0c44b2a4190fb0385d62ed6ea547d9c1e7406e6
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-10-26 18:23:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (31.83%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -4,14 +4,12 @@ public:
         int m = word1.size();
         int n = word2.size();
         string result = "";
-        int i = 0, j = 0;
-
-        while (i < m || j < n) {
-            if (i < m) {
-                result.push_back(word1[i++]);
+        for(int i=0;i<max(m,n);i++){
+            if(i<m){
+                result.push_back(word1[i]);
             }
-            if (j < n) {
-                result.push_back(word2[j++]);
+            if(i<n){
+                result.push_back(word2[i]);
             }
         }
 

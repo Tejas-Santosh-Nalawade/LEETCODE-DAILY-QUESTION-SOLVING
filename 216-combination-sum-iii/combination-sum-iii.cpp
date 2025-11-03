336f250e18d90e4834c446ce876ebf4e4d36f19c
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-11-03 23:49:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (42.33%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,21 +1,27 @@
 class Solution {
-public:
-  void combination(vector<vector<int>>& result, vector<int> sol, int k, int n) {
-    if (sol.size() == k && n == 0) { result.push_back(sol); return ; }
-    if (sol.size() < k) {
-      for (int i = sol.empty() ? 1 : sol.back() + 1; i <= 9; ++i) {
-        if (n - i < 0) break;
-        sol.push_back(i);
-        combination(result, sol, k, n - i);
-        sol.pop_back();
-      }
+private:
+    vector<int> tmp;
+    vector<vector<int>> res;
+    void backTrack(int cur, int k, int n) {
+        if (k == 1) {
+            if (n > cur && n <= 9) {
+                tmp.push_back(n);
+                res.push_back(tmp);
+                tmp.pop_back();
+            }
+            return;
+        }
+        int upper_bound = (n - k * (k - 1) / 2) / k;
+        for (int i = cur + 1; i <= min(upper_bound, 9); i++) {
+            tmp.push_back(i);
+            backTrack(i, k - 1, n - i);
+            tmp.pop_back();
+        }
     }
-  }
 
-  vector<vector<int>> combinationSum3(int k, int n) {
-    vector<vector<int>> result;
-    vector<int> sol;
-    combination(result, sol, k, n);
-    return result;
-  }
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        backTrack(0, k, n);
+        return res;
+    }
 };

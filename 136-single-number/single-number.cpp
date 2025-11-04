8aa8208badca79254143892417acdc6a3e4715dd
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-11-04 23:08:38 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (98.03%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) { 
-       unordered_map<int,int> a;
-	   for(auto x: nums)
-		   a[x]++;
-	   for(auto z:a)
-		   if(z.second==1)
-			   return z.first;
-	   return -1;
+       int ans=0;
+	   for(auto x:nums)
+	   ans^=x;
+	   return ans;
     }
 };

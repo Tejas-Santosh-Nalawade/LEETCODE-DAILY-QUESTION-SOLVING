7d447129b440a93c3f7bc4b8efcd72d8e0231989
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-11-09 21:51:08 +0530

    Time: 94 ms (5.03%) | Memory: 34.4 MB (91.27%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    vector<int> getElementsOnlyInFirstList(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> onlyInNums1;
-        
-        unordered_set<int> existsInNums2;
-        for (int num : nums2) {
-            existsInNums2.insert(num);
-        }
-        
-        for (int num : nums1) {
-            if (existsInNums2.find(num) == existsInNums2.end()) {
-                onlyInNums1.insert(num);
+    vector<int>getElement(vector<int>&nums1, vector<int>&nums2){
+        unordered_set<int> onlynum1;
+
+        for(int num : nums1){
+            bool existNum1 = false;
+            for(int x : nums2){
+                if(x == num){
+                    existNum1 = true;
+                    break;
+                }
+            }
+            if(!existNum1){
+                onlynum1.insert(num);
             }
         }
-        
-        return vector<int> (onlyInNums1.begin(), onlyInNums1.end());
+        return vector<int>(onlynum1.begin(),onlynum1.end());
     }
-    
+
+
+
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        return {getElementsOnlyInFirstList(nums1, nums2), getElementsOnlyInFirstList(nums2, nums1)};
+        return {getElement(nums1,nums2),getElement(nums2,nums1)};
     }
 };

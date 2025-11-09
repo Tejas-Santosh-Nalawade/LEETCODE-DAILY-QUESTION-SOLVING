dc50b7af5163b8ecc115de73cb9b3bc7562cee33
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-11-09 21:01:44 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (70.61%) - LeetSync

---
+++ 1009-pancake-sorting/pancake-sorting.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> pancakeSort(vector<int>& arr) {
+        vector<int> res;
+        int n = arr.size();
+
+        for(int curr = n; curr > 1; curr--){
+            int idx = find(arr.begin(), arr.end(), curr) - arr.begin();
+            if(idx == curr - 1) continue;
+            if(idx != 0){
+                reverse(arr.begin(), arr.begin() + idx + 1);
+                res.push_back(idx + 1);
+            }
+            reverse(arr.begin(), arr.begin() + curr);
+            res.push_back(curr);
+        }
+        return res;
+    }
+};

db6c5e23918ce89db608234a49888a4c25229ba9
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-11-10 00:22:12 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (40.22%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-        if(num1 == 0 && num2 == 0){
-            return 0;
+        // Euclidean Algorithm
+        int  res = 0;
+        while (num1 && num2){
+            res += num1 / num2;
+            num1 %=  num2;
+            swap(num1 , num2);
         }
-        if(num1 == num2){
-            return 1;
-        }
-        int count = 0;
-        while(num1 >0  && num2 >0){
-            if(num1 >= num2){
-                num1 -= num2;
-                count++;
-            }
-            else{
-                num2 -=num1;
-                count++;
-            }
-        }
-        return count;
-
+        return res;
     }
 };

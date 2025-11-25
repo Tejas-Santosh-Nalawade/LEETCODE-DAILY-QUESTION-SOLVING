a25a0007b4956c57dfae20bcf970b216202da59b
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-11-25 22:50:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (56.20%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,29 +1,49 @@
 class Solution {
 public:
-    std::string longestPalindrome(std::string s) {
-        if (s.length() <= 1) {
-            return s;
+    string longestPalindrome(string s) {
+        string s_prime = "#";
+        for (char c : s) {
+            s_prime += c;
+            s_prime += "#";
         }
-        
-        int max_len = 1;
-        int start = 0;
-        int end = 0;
-        std::vector<std::vector<bool>> dp(s.length(), std::vector<bool>(s.length(), false));
-        
-        for (int i = 0; i < s.length(); ++i) {
-            dp[i][i] = true;
-            for (int j = 0; j < i; ++j) {
-                if (s[j] == s[i] && (i - j <= 2 || dp[j + 1][i - 1])) {
-                    dp[j][i] = true;
-                    if (i - j + 1 > max_len) {
-                        max_len = i - j + 1;
-                        start = j;
-                        end = i;
-                    }
-                }
+
+        int n = s_prime.length();
+        vector<int> palindrome_radii(n, 0);
+        int center = 0;
+        int radius = 0;
+
+        for (int i = 0; i < n; i++) {
+            int mirror = 2 * center - i;
+
+            if (i < radius) {
+                palindrome_radii[i] = min(radius - i, palindrome_radii[mirror]);
+            }
+
+            while (i + 1 + palindrome_radii[i] < n &&
+                   i - 1 - palindrome_radii[i] >= 0 &&
+                   s_prime[i + 1 + palindrome_radii[i]] ==
+                       s_prime[i - 1 - palindrome_radii[i]]) {
+                palindrome_radii[i]++;
+            }
+
+            if (i + palindrome_radii[i] > radius) {
+                center = i;
+                radius = i + palindrome_radii[i];
             }
         }
-        
-        return s.substr(start, end - start + 1);
+
+        int max_length = 0;
+        int center_index = 0;
+        for (int i = 0; i < n; i++) {
+            if (palindrome_radii[i] > max_length) {
+                max_length = palindrome_radii[i];
+                center_index = i;
+            }
+        }
+
+        int start_index = (center_index - max_length) / 2;
+        string longest_palindrome = s.substr(start_index, max_length);
+
+        return longest_palindrome;
     }
 };

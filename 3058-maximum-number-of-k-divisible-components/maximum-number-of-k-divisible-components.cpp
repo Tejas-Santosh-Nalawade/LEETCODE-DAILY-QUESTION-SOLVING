cf2e539072283f11e20b0b3fc1acae0c10f15a6b
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-11-28 23:59:48 +0530

    Time: 135 ms (52.67%) | Memory: 178.1 MB (54.80%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -1,51 +1,33 @@
 class Solution {
 public:
-    int maxKDivisibleComponents(int n, vector<vector<int>>& edges,
-        vector<int>& values, int k) {
-        if (n < 2) return 1;
-
+    int maxKDivisibleComponents(int n, vector<vector<int>> &edges,
+                                vector<int> &values, int k) {
+        vector<int> adjList[n];
+        for (auto edge : edges) {
+            int node1 = edge[0];
+            int node2 = edge[1];
+            adjList[node1].push_back(node2);
+            adjList[node2].push_back(node1);
+        }
         int componentCount = 0;
-        vector<vector<int>> graph(n);
-        vector<int> inDegree(n, 0);
-        for (const auto& edge : edges) {
-            int node1 = edge[0], node2 = edge[1];
-            graph[node1].push_back(node2);
-            graph[node2].push_back(node1);
-            inDegree[node1]++;
-            inDegree[node2]++;
-        }
-        vector<long long> longValues(values.begin(), values.end());
-        queue<int> queue;
-        for (int node = 0; node < n; node++) {
-            if (inDegree[node] == 1) {
-                queue.push(node);
-            }
-        }
-
-        while (!queue.empty()) {
-            int currentNode = queue.front();
-            queue.pop();
-            inDegree[currentNode]--;
-
-            long long addValue = 0;
-            if (longValues[currentNode] % k == 0) {
-                componentCount++;
-            } else {
-                addValue = longValues[currentNode];
-            }
-            for (int neighborNode : graph[currentNode]) {
-                if (inDegree[neighborNode] == 0) {
-                    continue;
-                }
-
-                inDegree[neighborNode]--;
-                longValues[neighborNode] += addValue;
-                if (inDegree[neighborNode] == 1) {
-                    queue.push(neighborNode);
-                }
-            }
-        }
-
+        dfs(0, -1, adjList, values, k, componentCount);
         return componentCount;
+    }
+
+private:
+    int dfs(int currentNode, int parentNode, vector<int> adjList[],
+            vector<int> &nodeValues, int k, int &componentCount) {
+        int sum = 0;
+        for (auto neighborNode : adjList[currentNode]) {
+            if (neighborNode != parentNode) {
+                sum += dfs(neighborNode, currentNode, adjList, nodeValues, k,
+                           componentCount);
+                sum %= k;
+            }
+        }
+        sum += nodeValues[currentNode];
+        sum %= k;
+        if (sum == 0) componentCount++;
+        return sum;
     }
 };

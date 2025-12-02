cf98d5d39fa67f7b1a28d9e4a36b0eade8b026cd
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-12-02 23:24:19 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (84.92%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -6,11 +6,14 @@ public:
 
         int ballsToLeft = 0, movesToLeft = 0;
         int ballsToRight = 0, movesToRight = 0;
+
         for (int i = 0; i < n; i++) {
+            // Left pass
             answer[i] += movesToLeft;
             ballsToLeft += boxes[i] - '0';
             movesToLeft += ballsToLeft;
 
+            // Right pass
             int j = n - 1 - i;
             answer[j] += movesToRight;
             ballsToRight += boxes[j] - '0';

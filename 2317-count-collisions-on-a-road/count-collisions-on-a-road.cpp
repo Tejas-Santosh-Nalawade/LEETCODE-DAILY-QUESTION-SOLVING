a50303b3ff52018a967ff2bf4ad98cb8fbec403c
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-12-04 23:51:32 +0530

    Time: 10 ms (68.12%) | Memory: 19.2 MB (64.06%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int countCollisions(string directions) {
+        int n = directions.size();
+        int l = 0, r = n - 1;
+
+        while (l < n && directions[l] == 'L') {
+            l++;
+        }
+
+        while (r >= l && directions[r] == 'R') {
+            r--;
+        }
+
         int res = 0;
-        int flag = -1;
-        for (auto c : directions) {
-            if (c == 'L') {
-                if (flag >= 0) {
-                    res += flag + 1;
-                    flag = 0;
-                }
-            } else if (c == 'S') {
-                if (flag > 0) {
-                    res += flag;
-                }
-                flag = 0;
-            } else {
-                if (flag >= 0) {
-                    flag++;
-                } else {
-                    flag = 1;
-                }
+        for (int i = l; i <= r; i++) {
+            if (directions[i] != 'S') {
+                res++;
             }
         }
         return res;

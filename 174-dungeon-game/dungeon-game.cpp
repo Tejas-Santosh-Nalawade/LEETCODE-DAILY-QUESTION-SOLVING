2536db83a08006454242d5d3658d092c56a5abef
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-12-11 23:07:48 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (49.22%) - LeetSync

--- 174-dungeon-game/dungeon-game.cpp
+++ 174-dungeon-game/dungeon-game.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    int m,n;
-    int dp[201][201];
-    int solve(int i,int j,vector<vector<int>>&dungeon){
-        if(i==m || j==n) return 1e9;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(i==m-1 && j==n-1){
-            if(dungeon[i][j]>0){
-                return 1;
-            } else{
-                return abs(dungeon[i][j])+1;
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int n = dungeon.size();
+        int m = dungeon[0].size();
+        vector<vector<int>> dp(n, vector<int>(m));
+        dp[n-1][m-1] = max(1,1-dungeon[n-1][m-1]);
+        for (int i = n - 2; i >= 0; i--) {
+            dp[i][m-1] = max(1,dp[i+1][m-1]-dungeon[i][m-1]);
+        }
+        for (int j = m - 2; j >= 0; j--) {
+            dp[n-1][j] = max(1,dp[n-1][j+1]-dungeon[n-1][j]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = m - 2; j >= 0; j--) {
+                dp[i][j] = max(1,min(dp[i+1][j],dp[i][j+1]) - dungeon[i][j]);
             }
         }
-        int down=solve(i+1,j,dungeon);
-        int right=solve(i,j+1,dungeon);
-
-        int ans=min(down,right)-dungeon[i][j];
-
-        return dp[i][j]=ans>0?ans:1;
-    }
-    int calculateMinimumHP(vector<vector<int>>& dungeon) {
-        m=dungeon.size(),n=dungeon[0].size();
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,dungeon);
+        return dp[0][0];
     }
 };

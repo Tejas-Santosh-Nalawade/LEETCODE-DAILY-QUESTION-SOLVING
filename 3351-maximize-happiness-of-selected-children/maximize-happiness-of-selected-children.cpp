a34436ca181b1fe3551edd90d3a0c7d8c8f5f667
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2025-12-25 13:29:53 +0530

    Time: 185 ms (27.31%) | Memory: 117.4 MB (5.67%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        sort(happiness.begin(), happiness.end(), greater<int>());
+        priority_queue<int>pq;
+
+        for(const auto& h: happiness){
+            pq.push(h);
+        }
+
         long long ans = 0;
         int turns = 0;
+
         for(int i=0; i<k; i++){
-           ans +=max(happiness[i] - turns , 0);
-           turns++; 
+            ans += max(pq.top() - turns , 0);
+
+            pq.pop();
+
+            turns++;
         }
         return ans;
     }

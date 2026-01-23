b585ce9b94a26b2ee6136ad0449bfa0f104d0b8a
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-01-23 16:36:40 +0530

    Time: 468 ms (83.54%) | Memory: 163 MB (93.04%) - LeetSync

---
+++ 3772-minimum-pair-removal-to-sort-array-ii/minimum-pair-removal-to-sort-array-ii.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    int minimumPairRemoval(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 1) return 0;
+
+        vector<long long> a(nums.begin(), nums.end());
+        vector<int> left(n), right(n);
+        vector<bool> alive(n, true);
+
+        for (int i = 0; i < n; i++) {
+            left[i] = i - 1;
+            right[i] = i + 1;
+        }
+        right[n - 1] = -1;
+
+        priority_queue<
+            pair<long long,int>,
+            vector<pair<long long,int>>,
+            greater<>
+        > pq;
+
+        for (int i = 0; i < n - 1; i++) {
+            pq.push({a[i] + a[i + 1], i});
+        }
+
+        int inv = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (a[i] > a[i + 1]) inv++;
+        }
+
+        int ops = 0;
+
+        while (inv > 0) {
+            auto [sum, i] = pq.top();
+            pq.pop();
+
+            int j = right[i];
+            if (j == -1 || !alive[i] || !alive[j] || a[i] + a[j] != sum)
+                continue;
+
+            int L = left[i], R = right[j];
+
+            if (L != -1 && a[L] > a[i]) inv--;
+            if (a[i] > a[j]) inv--;
+            if (R != -1 && a[j] > a[R]) inv--;
+
+            a[i] = sum;
+            alive[j] = false;
+            right[i] = R;
+            if (R != -1) left[R] = i;
+
+            if (L != -1 && a[L] > a[i]) inv++;
+            if (R != -1 && a[i] > a[R]) inv++;
+
+            if (L != -1) pq.push({a[L] + a[i], L});
+            if (R != -1) pq.push({a[i] + a[R], i});
+
+            ops++;
+        }
+
+        return ops;
+    }
+};

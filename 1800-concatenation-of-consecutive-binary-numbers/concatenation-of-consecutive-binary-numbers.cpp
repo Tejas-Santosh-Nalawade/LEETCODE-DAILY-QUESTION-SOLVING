e809db878f41d143721af5c0957fa05c7bddf579
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-02-28 23:09:28 +0530

    Time: 44 ms (59.06%) | Memory: 8.1 MB (41.61%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int concatenatedBinary(int n) {
-        long long  result = 0;
+        long long result = 0;
         int digits = 0;
-        int MOD = 1e9+7;
+        int MOD = 1000000007;
         for(int num = 1; num<=n; num++){
             if((num & (num - 1)) == 0){
                 digits++;

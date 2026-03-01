0905c3f9ce7f0cbba35b6563e5cf86da99c9dd55
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-03-01 13:02:00 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (16.53%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int minPartitions(string n) {
-        int len = n.length();
-        int max_digit_in_string = 0;
-        
-        for(auto element : n)
-        {
-            // convert the string number into integer number by subtracting the ascii value.
-            int digit = element - '0';
-            max_digit_in_string = max(max_digit_in_string,digit);
+        int max_value = 0;
+
+        for(auto c: n){
+            int digit = c - '0';
+            max_value = max(max_value , digit);
         }
-        
-        return max_digit_in_string;
+        return max_value;
+
     }
 };

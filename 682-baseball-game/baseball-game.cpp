c92fa3c50dfd2510053ff465d36c8bd7d1909994
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-03-02 20:59:14 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (66.83%) - LeetSync

---
+++ 682-baseball-game/baseball-game.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+  int calPoints(vector<string> &operations) {
+    vector<int> ans;
+
+    for (auto &ch : operations) {
+      if (ch == "+") {
+        int n = ans.size();
+        ans.push_back(ans[n - 1] + ans[n - 2]);
+      } else if (ch == "D") {
+        if (!ans.empty()) {
+          int val = ans.back();
+          ans.push_back(2 * val);
+        }
+      } else if (ch == "C") {
+        if (!ans.empty()) {
+          ans.pop_back();
+        }
+      } else
+        ans.push_back(stoi(ch));
+    }
+
+    int sum = accumulate(ans.begin(), ans.end(), 0);
+    return sum;
+  }
+};

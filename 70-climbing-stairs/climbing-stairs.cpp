68033325c1a359945e48206bbe7857868d070bf5
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-07-15 14:28:36 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (57.80%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 1)
-            return 1;
-
         int a = 1, b = 1;
-
         for (int i = 2; i <= n; i++) {
-            int c = a + b;
-            a = b;
-            b = c;
+            b = a + b;
+            swap(a,b);
         }
 
-        return b;
+        return a;
     }
 };

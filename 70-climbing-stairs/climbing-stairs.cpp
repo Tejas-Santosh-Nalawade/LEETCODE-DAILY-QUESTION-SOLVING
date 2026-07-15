3a09bca0833f2bed93f9475c368cd427987209bd
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-07-15 14:20:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (19.82%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if (n == 0 || n == 1) {
+    vector<int> dp;
+
+    int solve(int n) {
+        if (n <= 1)
             return 1;
-        }
-        int prev = 1, curr = 1;
-        for (int i = 2; i <= n; i++) {
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
-        }
-        return curr;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        return dp[n] = solve(n - 1) + solve(n - 2);
+    }
+
+    int climbStairs(int n) {
+        dp.resize(n + 1, -1);
+        return solve(n);
     }
 };

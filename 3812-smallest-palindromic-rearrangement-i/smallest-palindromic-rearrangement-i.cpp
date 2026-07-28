763c16793565ca8d6b94892a51f146d9d0008680
Tejas Nalawade <89332752+Tejas-Santosh-Nalawade@users.noreply.github.com>
2026-07-28 12:02:56 +0530

    Time: 26 ms (75.82%) | Memory: 54.3 MB (88.13%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     string smallestPalindrome(string s) {
-        int len = s.length();
-        int part = len / 2;
-        sort(s.begin(), s.begin() + part);
-        for(int i=0; i<part; i++){
-            s[len - 1 - i] = s[i];
+        int n = s.length();
+        int bucket[26] = {0};
+
+        for (int i = 0; i < n / 2; i++) {
+            bucket[s[i] - 'a']++;
         }
+
+        int left = 0;
+        int right = n - 1;
+
+        for (int i = 0; i < 26; i++) {
+            while (bucket[i] > 0) {
+                char c = i + 'a';
+                s[left++] = c;
+                s[right--] = c;
+                bucket[i]--;
+            }
+        }
+
         return s;
     }
 };
